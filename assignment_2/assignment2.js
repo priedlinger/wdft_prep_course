6d4310f7996bcbf5a63825665c21d0f0c2b0583c
Paul Riedlinger <paul@riedlinger.ca>
2017-10-22 22:42:01 -0400

    Commented the program for the hashtag tree

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -24,3 +24,8 @@ while (count.length <=7) {
     console.log(count);
     count=count + "#";
 }
+
+/* the basic explanation of the functioning of the program.   You initalize the variable count as a string with the single hashtag in it.   The using a While loop,
+you first check the length of the count variable and make sure it is less than or equal to 7.   If it is you use the console.log command to output count and then add
+(or append) another hashtage to the count variable string.   Once the variable count has a length of greater than 7 the program will stop executing the while loop
+*/
